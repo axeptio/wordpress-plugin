2dc7bb098c24771fcc6ef00eb9751df52612b5aa
Florian Davreux <dectys@gmail.com>
2022-10-03 17:53:12 +0200

    remove typo and obsolete text_domain

--- axeptio-wordpress-plugin/inc/Base/BaseController.php
+++ axeptio-wordpress-plugin/inc/Base/BaseController.php
@@ -10,13 +10,11 @@ class BaseController
 {
     public $plugin_path;
     public $plugin_url;
-    public $plugin_;
-    public $text_domain;
+    public $plugin;
     
     public function __construct(){
         $this->plugin_path = plugin_dir_path( dirname(__FILE__) );
         $this->plugin_url = plugin_dir_url( dirname(__FILE__) );
         $this->plugin = plugin_basename( dirname(__FILE__) ) . '/axeptio-wordpress-plugin.php';
-        $this->text_domain = 'axeptio-wordpress-plugin';
     }
 }
