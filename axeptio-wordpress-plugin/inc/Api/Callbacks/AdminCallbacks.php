b354b406644f275e35892bdbde00c4c36108848b
Florian Davreux <dectys@gmail.com>
2022-10-04 10:01:52 +0200

    more explicit escaped value

--- axeptio-wordpress-plugin/inc/Api/Callbacks/AdminCallbacks.php
+++ axeptio-wordpress-plugin/inc/Api/Callbacks/AdminCallbacks.php
@@ -22,17 +22,17 @@ class AdminCallbacks extends BaseController{
 	}
 	public function xpwpSdkActiveSet()
 	{
-		$value = esc_attr( get_option( 'xpwp_sdk_active' ) );
-		echo '<input type="checkbox" class="regular-text" name="xpwp_sdk_active" value="1" ' . ($value == 1 ? 'checked' : '') . ' placeholder="">';
+		$escaped_value = esc_attr( get_option( 'xpwp_sdk_active' ) );
+		echo '<input type="checkbox" class="regular-text" name="xpwp_sdk_active" value="1" ' . ($escaped_value == 1 ? 'checked' : '') . ' placeholder="">';
 	}
 	public function xpwpClientIdSet()
 	{
-		$value = esc_attr( get_option( 'xpwp_client_id' ) );
-		echo '<input type="text" class="regular-text" name="xpwp_client_id" id ="xpwp_client_id" value="' . $value . '" placeholder="" onchange="loadVersionsOnChange()">';
+		$escaped_value = esc_attr( get_option( 'xpwp_client_id' ) );
+		echo '<input type="text" class="regular-text" name="xpwp_client_id" id ="xpwp_client_id" value="' . $escaped_value . '" placeholder="" onchange="loadVersionsOnChange()">';
 	}
 	public function xpwpVersionSet()
 	{
-		$value = esc_attr( get_option( 'xpwp_version' ) );
-		echo '<select name="xpwp_version" id="xpwp_version"><option value="' . $value . '">' . $value . '</option></select>';
+		$escaped_value = esc_attr( get_option( 'xpwp_version' ) );
+		echo '<select name="xpwp_version" id="xpwp_version"><option value="' . $escaped_value . '">' . $escaped_value . '</option></select>';
 	}
 }
