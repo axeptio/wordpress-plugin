42fdcf97c3ded991a4e336cb585c976377b4d569
Olivier Gorzalka <olivier@amphibee.fr>
2023-04-20 17:36:17 +0200

    Rename label option

--- templates/admin/fields/main/version.php
+++ templates/admin/fields/main/version.php
@@ -4,7 +4,7 @@
 		name="xpwp_version"
 		class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-amber-400 sm:max-w-xs sm:text-sm sm:leading-6"
 	>
-		<option value=""><?php esc_html_e( 'Select a version', 'axeptio-wordpress-plugin' ); ?></option>
+		<option value=""><?php esc_html_e( 'Dynamic (Axeptio based)', 'axeptio-wordpress-plugin' ); ?></option>
 		<template x-for="option in options" :key="option.value">
 			<option
 				:value="option.value"
