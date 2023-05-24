6ceaf13e924878477cc110eb7768bb4b658c3f1a
Olivier Gorzalka <olivier@amphibee.fr>
2023-05-24 07:27:39 +0200

    🚀 chore(axeptio.js, axeptio-wordpress-plugin.php, class-migration-2.0.2.php, readme.txt): update version to 2.0.2 The version number has been updated to 2.0.2 in the plugin files and readme.txt file. The migration class Migration_2_0_2 has been added to update the database schema to add a default value ('wordpress'). The axeptio.js file has been updated to remove the code that adds vendors to the first WordPress step.

---
+++ includes/classes/migrations/class-migration-2.0.2.php
@@ -0,0 +1,36 @@
+<?php
+namespace Axeptio\Migrations;
+
+use Axeptio\Models\Plugins;
+
+class Migration_2_0_2 implements \Axeptio\Contracts\Migration_Interface {
+	/**
+	 * Run the upgrade migration.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		global $wpdb;
+
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
+		$table = $wpdb->prefix . Plugins::$table_name;
+
+		$wpdb->query( // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
+			$wpdb->prepare(
+				"ALTER TABLE %s ALTER COLUMN cookie_widget_step SET DEFAULT 'wordpress';", // phpcs:ignore WordPress.DB.DirectDatabaseQuery.SchemaChange, WordPress.WP.CapitalPDangit.Misspelled
+				$table
+			)
+		);
+
+		$sql = "UPDATE `$table` SET `cookie_widget_step` = 'wordpress' WHERE cookie_widget_step IS NULL OR cookie_widget_step = 0;"; // phpcs:ignore WordPress.WP.CapitalPDangit.Misspelled
+		dbDelta( $sql );
+	}
+
+	/**
+	 * Run the downgrade migration.
+	 *
+	 * @return void
+	 */
+	public function down() {    }
+}
