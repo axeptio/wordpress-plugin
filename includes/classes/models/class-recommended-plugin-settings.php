77d78c47fa377ae392719790a5d8536ef33fced2
Olivier Gorzalka <olivier@amphibee.fr>
2023-07-20 12:01:44 +0200

    🐛 fix(class-recommended-plugin-settings.php): fix typo in return type declaration from 'falsee' to 'false' 🐛 fix(class-recommended-plugin-settings.php): fix condition in format_datas() method to check if 'hooks' key is not set in $datas array instead of checking if $datas array is empty

--- includes/classes/models/class-recommended-plugin-settings.php
+++ includes/classes/models/class-recommended-plugin-settings.php
@@ -108,7 +108,7 @@ class Recommended_Plugin_Settings {
 	 * Format the data.
 	 *
 	 * @param array $datas Data to format.
-	 * @return array
+	 * @return array|false
 	 */
 	public static function format_datas( array $datas ) {
 		$settings = array(
@@ -118,7 +118,7 @@ class Recommended_Plugin_Settings {
 			'shortcode_tags_list' => array(),
 		);
 
-		if ( count( $datas ) === 0 ) {
+		if ( !isset( $datas['hooks'] ) ) {
 			return false;
 		}
 
