21f78849a1dc4f3b13a6140f604f3297f32a542e
Olivier Gorzalka <olivier@amphibee.fr>
2024-03-19 16:17:45 +0100

    Fix Google Consent Mode Settings

--- includes/classes/frontend/class-axeptio-sdk.php
+++ includes/classes/frontend/class-axeptio-sdk.php
@@ -19,15 +19,18 @@ use function Axeptio\script_url;
 use function Axeptio\style_url;
 use function Axeptio\Utility\get_asset_info;
 
-class Axeptio_Sdk extends Module {
+class Axeptio_Sdk extends Module
+{
 
 	const OPTION_JSON_COOKIE_NAME = 'axeptio_cookies';
+
 	/**
 	 * Module can run within the current context.
 	 *
 	 * @return true
 	 */
-	public function can_register() {
+	public function can_register()
+	{
 		return true;
 	}
 
@@ -36,8 +39,9 @@ class Axeptio_Sdk extends Module {
 	 *
 	 * @return void
 	 */
-	public function register() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+	public function register()
+	{
+		add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
 	}
 
 	/**
@@ -45,76 +49,77 @@ class Axeptio_Sdk extends Module {
 	 *
 	 * @return void
 	 */
-	public function enqueue_scripts() {
+	public function enqueue_scripts()
+	{
 		$settings = $this->get_sdk_settings();
 
-		if ( ! $settings ) {
+		if (!$settings) {
 			return;
 		}
 
 		wp_enqueue_style(
 			'axeptio/main',
-			style_url( 'frontend/main', 'frontend' ),
+			style_url('frontend/main', 'frontend'),
 			array(),
-			get_asset_info( 'shared', 'version' ),
+			get_asset_info('shared', 'version'),
 		);
 
-		$cookies_version = Settings::get_option( 'version', false );
+		$cookies_version = Settings::get_option('version', false);
 		$cookies_version = '' === $cookies_version ? 'all' : $cookies_version;
 
 		$wordpress_vendors = array_values(
 			array_filter(
 				array_map(
-					function ( $plugin_configuration ) use ( $cookies_version ) {
+					function ($plugin_configuration) use ($cookies_version) {
 
-						$configuration = 'all' !== $cookies_version && isset( $plugin_configuration['Metas']['Merged'] ) ? $plugin_configuration['Metas']['Merged'] : $plugin_configuration['Metas'];
+						$configuration = 'all' !== $cookies_version && isset($plugin_configuration['Metas']['Merged']) ? $plugin_configuration['Metas']['Merged'] : $plugin_configuration['Metas'];
 
-						if ( ! isset( $configuration['enabled'] ) || ! (bool) $configuration['enabled'] ) {
-								return false;
+						if (!isset($configuration['enabled']) || !(bool)$configuration['enabled']) {
+							return false;
 						}
 
 						return array(
-							'name'             => "wp_{$configuration['plugin']}" ?? '',
-							'title'            => isset( $configuration['vendor_title'] ) && '' !== $configuration['vendor_title'] ? $configuration['vendor_title'] : $plugin_configuration['Name'],
-							'shortDescription' => isset( $configuration['vendor_shortDescription'] ) && '' !== $configuration['vendor_shortDescription'] ? wp_strip_all_tags( $configuration['vendor_shortDescription'] ) : $plugin_configuration['Description'],
-							'longDescription'  => wp_strip_all_tags( $configuration['vendor_longDescription'] ?? '' ),
-							'policyUrl'        => isset( $configuration['vendor_policyUrl'] ) && '' !== $configuration['vendor_policyUrl'] ? $configuration['vendor_policyUrl'] : $plugin_configuration['PluginURI'],
+							'name' => "wp_{$configuration['plugin']}" ?? '',
+							'title' => isset($configuration['vendor_title']) && '' !== $configuration['vendor_title'] ? $configuration['vendor_title'] : $plugin_configuration['Name'],
+							'shortDescription' => isset($configuration['vendor_shortDescription']) && '' !== $configuration['vendor_shortDescription'] ? wp_strip_all_tags($configuration['vendor_shortDescription']) : $plugin_configuration['Description'],
+							'longDescription' => wp_strip_all_tags($configuration['vendor_longDescription'] ?? ''),
+							'policyUrl' => isset($configuration['vendor_policyUrl']) && '' !== $configuration['vendor_policyUrl'] ? $configuration['vendor_policyUrl'] : $plugin_configuration['PluginURI'],
 							// TODO: Vendor Domain.
-							'domain'           => $configuration['vendor_domain'] ?? '',
-							'image'            => '' === $configuration['vendor_image'] && isset( $configuration['Merged']['vendor_image'] ) ? $configuration['Merged']['vendor_image'] : $configuration['vendor_image'],
-							'type'             => 'wordpress plugin',
-							'step'             => $configuration['cookie_widget_step'] ?? 'wordpress',
+							'domain' => $configuration['vendor_domain'] ?? '',
+							'image' => '' === $configuration['vendor_image'] && isset($configuration['Merged']['vendor_image']) ? $configuration['Merged']['vendor_image'] : $configuration['vendor_image'],
+							'type' => 'wordpress plugin',
+							'step' => $configuration['cookie_widget_step'] ?? 'wordpress',
 						);
 					},
-					Plugins::all( $cookies_version )
+					Plugins::all($cookies_version)
 				)
 			)
 		);
 
 		wp_enqueue_script(
 			'axeptio/sdk-script',
-			script_url( 'frontend/axeptio', 'frontend' ),
+			script_url('frontend/axeptio', 'frontend'),
 			array(),
 			XPWP_VERSION,
 			true
 		);
 
-		wp_localize_script( 'axeptio/sdk-script', 'Axeptio_SDK', $settings );
-		wp_localize_script( 'axeptio/sdk-script', 'axeptioWordpressVendors', $wordpress_vendors );
-		wp_localize_script( 'axeptio/sdk-script', 'axeptioWordpressSteps', Axeptio_Steps::all() );
+		wp_localize_script('axeptio/sdk-script', 'Axeptio_SDK', $settings);
+		wp_localize_script('axeptio/sdk-script', 'axeptioWordpressVendors', $wordpress_vendors);
+		wp_localize_script('axeptio/sdk-script', 'axeptioWordpressSteps', Axeptio_Steps::all());
 
-		$sdk_script = \Axeptio\get_template_part( 'frontend/sdk', array(), false );
-		preg_match( '/<script[^>]*>(.*?)<\/script>/is', $sdk_script, $matches );
-		wp_add_inline_script( 'axeptio/sdk-script', $matches[1] ?? '' );
+		$sdk_script = \Axeptio\get_template_part('frontend/sdk', array(), false);
+		preg_match('/<script[^>]*>(.*?)<\/script>/is', $sdk_script, $matches);
+		wp_add_inline_script('axeptio/sdk-script', $matches[1] ?? '');
 
 		add_action(
 			'wp_head',
-			function () use ( $settings ) {
+			function () use ($settings) {
 				\Axeptio\get_template_part(
 					'frontend/google-consent-mode',
 					array(
-						'active_google_consent_mode' => (bool) $settings['googleConsentMode'],
-						'google_consent_mode_params' => $settings['googleConsentModeParams'],
+						'active_google_consent_mode' => (bool)$settings['enableGoogleConsentMode'],
+						'google_consent_mode_params' => $settings['googleConsentMode'],
 					)
 				);
 			}
@@ -126,42 +131,45 @@ class Axeptio_Sdk extends Module {
 	 *
 	 * @return array|false Settings of the SDK.
 	 */
-	private function get_sdk_settings() {
-		$sdk_active         = Sdk::is_active();
-		$disable_send_datas = (bool) Settings::get_option( 'disable_send_datas', false );
+	private function get_sdk_settings()
+	{
+		$sdk_active = Sdk::is_active();
+		$disable_send_datas = (bool)Settings::get_option('disable_send_datas', false);
 
-		$client_id       = Settings::get_option( 'client_id', false );
+		$client_id = Settings::get_option('client_id', false);
 		$cookies_version = Project_Versions::get_current_lang_version();
 
-		$google_consent_mode        = Settings::get_option( 'google_consent_mode', '0' );
+		$google_consent_mode = Settings::get_option('google_consent_mode', '0');
 		$google_consent_mode_params = Settings::get_option(
 			'google_consent_params',
 			array(
-				'analytics_storage'  => false,
-				'ad_storage'         => false,
-				'ad_user_data'       => false,
+				'analytics_storage' => false,
+				'ad_storage' => false,
+				'ad_user_data' => false,
 				'ad_personalization' => false,
 			)
-			);
+		);
 
-		if ( ! $sdk_active || ( ! $client_id && ! $cookies_version ) ) {
+		if (!$sdk_active || (!$client_id && !$cookies_version)) {
 			return false;
 		}
 
 		$sdk_settings = array(
-			'clientId'                => $client_id,
-			'platform'                => 'plugin-wordpress',
-			'sendDatas'               => $disable_send_datas,
-			'googleConsentMode'       => $google_consent_mode,
-			'googleConsentModeParams' => array(
-				'analytics_storage'  => isset( $google_consent_mode_params['analytics_storage'] ) && '1' === $google_consent_mode_params['analytics_storage'] ? 'granted' : 'denied',
-				'ad_storage'         => isset( $google_consent_mode_params['ad_storage'] ) && '1' === $google_consent_mode_params['ad_storage'] ? 'granted' : 'denied',
-				'ad_user_data'       => isset( $google_consent_mode_params['ad_user_data'] ) && '1' === $google_consent_mode_params['ad_user_data'] ? 'granted' : 'denied',
-				'ad_personalization' => isset( $google_consent_mode_params['ad_personalization'] ) && '1' === $google_consent_mode_params ? 'granted' : 'denied',
+			'clientId' => $client_id,
+			'platform' => 'plugin-wordpress',
+			'sendDatas' => $disable_send_datas,
+			'enableGoogleConsentMode' => $google_consent_mode,
+			'googleConsentMode' => array(
+				'default' => array(
+					'analytics_storage' => isset($google_consent_mode_params['analytics_storage']) && '1' === $google_consent_mode_params['analytics_storage'] ? 'granted' : 'denied',
+					'ad_storage' => isset($google_consent_mode_params['ad_storage']) && '1' === $google_consent_mode_params['ad_storage'] ? 'granted' : 'denied',
+					'ad_user_data' => isset($google_consent_mode_params['ad_user_data']) && '1' === $google_consent_mode_params['ad_user_data'] ? 'granted' : 'denied',
+					'ad_personalization' => isset($google_consent_mode_params['ad_personalization']) && '1' === $google_consent_mode_params ? 'granted' : 'denied',
+				),
 			),
 		);
 
-		if ( '' !== $cookies_version ) {
+		if ('' !== $cookies_version) {
 			$sdk_settings['cookiesVersion'] = $cookies_version;
 		}
 
