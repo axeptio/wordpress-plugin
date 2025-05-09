5a54c847a5a5f91044cfde2d67e20e6d3620ed5d
lboulanger <louis.boulanger.work@gmail.com>
2025-05-09 10:55:44 +0200

    ♻️ Refactor webpack.mix.js to remove unused ImageminPlugin and simplify configuration and Upgrade devDependencies versions for better compatibility and security

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,28 +1,14 @@
 let mix = require('laravel-mix');
-let ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 mix.browserSync({
 	proxy: 'https://axeptio.ddev.site',
 	https: true,
-	files: [
-		'./templates/**/*.php',
-		'./includes/**/*.php',
-		'./dist/js/**/*.js',
-		'./dist/css/**/*.css',
-	],
+	files: ['./templates/**/*.php', './includes/**/*.php', './dist/js/**/*.js', './dist/css/**/*.css']
 });
 
 mix.webpackConfig({
-	plugins: [
-		new ImageminPlugin({
-			// disable: process.env.NODE_ENV !== 'production', // Disable during development
-			pngquant: {
-				quality: '95-100',
-			},
-			test: /\.(jpe?g|png|gif|svg)$/i,
-		}),
-	]
-})
+	plugins: []
+});
 
 /*
  |--------------------------------------------------------------------------
@@ -41,15 +27,12 @@ mix
 	.copy('assets/fonts', 'dist/fonts')
 	.js('assets/js/backend/app.js', 'dist/js/backend/app.js')
 	.js('assets/js/frontend/axeptio.js', 'dist/js/frontend/axeptio.js')
-	.postCss("assets/css/backend/main.css", "dist/css/backend/", [
+	.postCss('assets/css/backend/main.css', 'dist/css/backend/', [
 		require('postcss-nested'),
 		require('autoprefixer'),
-		require('tailwindcss'),
+		require('tailwindcss')
 	])
-	.postCss("assets/css/frontend/main.css", "dist/css/frontend/", [
-		require('postcss-nested'),
-		require('autoprefixer'),
-	])
+	.postCss('assets/css/frontend/main.css', 'dist/css/frontend/', [require('postcss-nested'), require('autoprefixer')])
 	.options({
 		processCssUrls: false
 	});
